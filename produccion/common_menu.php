d5605c78c819bc02acdcbaf5174fff1519abe3f8
Your Name <you@example.com>
2016-05-31 16:19:27 -0400

    Cambios en el dashboar para agregar el modulo y la permisología de ventas y ofertas, así como las tablas de BD que se agregaron y modificaron

--- produccion/common_menu.php
+++ produccion/common_menu.php
@@ -40,6 +40,22 @@
                   </li>
                   <?php } ?>
 
+                  <?php if (control_access("OFERTAS", 'VER')) { ?>
+                
+                  <li><a><i class="fa fa-tags"></i> Ofertas  <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <?php if (control_access("OFERTAS", 'AGREGAR')) { ?>
+                      <li><a href="<?=$serveractual?>/produccion/ofertas/index.php"><i class="fa fa-plus-square-o"></i>Agregar</a>
+                      </li>
+                      <?php } ?>
+                      <?php if (control_access("OFERTAS", 'VER')) { ?>
+                      <li><a href="<?=$serveractual?>/produccion/ofertas/listar.php"><i class="fa fa-database"></i>Listar</a>
+                      </li>
+                      <?php } ?>
+                    </ul>
+                  </li>
+                  <?php } ?>
+
                    <?php if (control_access("CATEGORIAS", 'VER')) { ?>
                 
                   <li><a><i class="fa fa-tasks"></i> Categorías  <span class="fa fa-chevron-down"></span></a>
@@ -56,66 +72,25 @@
                   </li>
                   <?php } ?>
 
-                  <li><a><i class="fa fa-edit"></i> Forms <span class="fa fa-chevron-down"></span></a>
+
+
+                   <?php if (control_access("VENTAS", 'VER')) { ?>
+                
+                  <li><a><i class="fa fa-shopping-cart"></i> Ventas  <span class="fa fa-chevron-down"></span></a>
                     <ul class="nav child_menu">
-                      <li><a href="form.html">General Form</a>
+                      <li><a href="<?=$serveractual?>/produccion/ventas/index.php"><i class="fa fa-ticket"></i>Ventas por Oferta</a>
                       </li>
-                      <li><a href="form_advanced.html">Advanced Components</a>
+                      <li><a href="<?=$serveractual?>/produccion/ventas/index.php"><i class="fa fa-bar-chart"></i>Reporte</a>
                       </li>
-                      <li><a href="form_validation.html">Form Validation</a>
-                      </li>
-                      <li><a href="form_wizards.html">Form Wizard</a>
-                      </li>
-                      <li><a href="form_upload.html">Form Upload</a>
-                      </li>
-                      <li><a href="form_buttons.html">Form Buttons</a>
-                      </li>
+                      
+                      
+                                           
                     </ul>
                   </li>
-                  <li><a><i class="fa fa-desktop"></i> UI Elements <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="general_elements.html">General Elements</a>
-                      </li>
-                      <li><a href="media_gallery.html">Media Gallery</a>
-                      </li>
-                      <li><a href="typography.html">Typography</a>
-                      </li>
-                      <li><a href="icons.html">Icons</a>
-                      </li>
-                      <li><a href="glyphicons.html">Glyphicons</a>
-                      </li>
-                      <li><a href="widgets.html">Widgets</a>
-                      </li>
-                      <li><a href="invoice.html">Invoice</a>
-                      </li>
-                      <li><a href="inbox.html">Inbox</a>
-                      </li>
-                      <li><a href="calendar.html">Calendar</a>
-                      </li>
-                    </ul>
-                  </li>
-                  <li><a><i class="fa fa-table"></i> Tables <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="tables.html">Tables</a>
-                      </li>
-                      <li><a href="tables_dynamic.html">Table Dynamic</a>
-                      </li>
-                    </ul>
-                  </li>
-                  <li><a><i class="fa fa-bar-chart-o"></i> Data Presentation <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="chartjs.html">Chart JS</a>
-                      </li>
-                      <li><a href="chartjs2.html">Chart JS2</a>
-                      </li>
-                      <li><a href="morisjs.html">Moris JS</a>
-                      </li>
-                      <li><a href="echarts.html">ECharts </a>
-                      </li>
-                      <li><a href="other_charts.html">Other Charts </a>
-                      </li>
-                    </ul>
-                  </li>
+                  <?php } ?>
+
+               
+
                 </ul>
               </div>
            
